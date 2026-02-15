489ac6ab2501cdc4fe4267c3a763bcb5a833f8ee
chinobetoska <robertojuarezdelgadillo@gmail.com>
2026-02-14 22:00:20 -0600

    conexion real a servidor deral con render

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,5 @@
 async function cargarDestinos() {
-    //const res = await fetch('http://localhost:3000/api/destinos');
+    const res = await fetch('https://nenemi-a-full-version.onrender.com/destinos');
     const datos = await res.json();
     
     const contenedor = document.getElementById('caja-servidor');

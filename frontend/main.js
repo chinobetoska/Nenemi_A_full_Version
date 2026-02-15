f3b606b34e9718ce055f0266015649f83ffd3baf
chinobetoska <robertojuarezdelgadillo@gmail.com>
2026-02-14 22:13:17 -0600

    cabios de urls

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,5 @@
 async function cargarDestinos() {
-    const res = await fetch('https://nenemi-a-full-version.onrender.com/destinos');
+    const res = await fetch('https://nenemi-a-full-version.onrender.com/api/destinos');
     const datos = await res.json();
     
     const contenedor = document.getElementById('caja-servidor');
